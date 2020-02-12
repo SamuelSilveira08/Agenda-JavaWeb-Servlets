917ad38045b60dc4597be296cdbd6fff39075c5e
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-12 13:56:00 -0300

    Implemented CRUD methods

--- Agenda-Repositorios/src/br/com/agenda/repositorios/impl/ContatoRepositorio.java
+++ Agenda-Repositorios/src/br/com/agenda/repositorios/impl/ContatoRepositorio.java
@@ -1,31 +1,36 @@
 package br.com.agenda.repositorios.impl;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import br.com.agenda.entidades.Contato;
 import br.com.agenda.repositorios.interfaces.AgendaRepositorio;
 
 public class ContatoRepositorio implements AgendaRepositorio<Contato> {
 
+	private static List<Contato> contatos = new ArrayList<Contato>();
+
 	@Override
 	public List<Contato> selectAll() {
-		
-		return null;
+		return contatos;
 	}
 
 	@Override
 	public void save(Contato entity) {
-		
+		contatos.add(entity);
 	}
 
 	@Override
 	public void delete(Contato entity) {
-		
+		contatos.remove(entity);
 	}
 
 	@Override
 	public void update(Contato entity) {
-		
+		var original = contatos.stream().filter(contato -> contato.getNome().equals(entity.getNome())).findFirst();
+		if (original.isPresent()) {
+			original.get().setIdade(entity.getIdade());
+			original.get().setTelefone(entity.getTelefone());
+		}
 	}
 
 }
