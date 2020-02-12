c1c0d0ab915d276d174a9b618334a6f7490a7fb8
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-12 13:23:25 -0300

    Added impl package and ContatoRepositorio class, which implements AgendaRepositorio interface

---
+++ Agenda-Repositorios/src/br/com/agenda/repositorios/impl/ContatoRepositorio.java
@@ -0,0 +1,31 @@
+package br.com.agenda.repositorios.impl;
+
+import java.util.List;
+
+import br.com.agenda.entidades.Contato;
+import br.com.agenda.repositorios.interfaces.AgendaRepositorio;
+
+public class ContatoRepositorio implements AgendaRepositorio<Contato> {
+
+	@Override
+	public List<Contato> selectAll() {
+		
+		return null;
+	}
+
+	@Override
+	public void save(Contato entity) {
+		
+	}
+
+	@Override
+	public void delete(Contato entity) {
+		
+	}
+
+	@Override
+	public void update(Contato entity) {
+		
+	}
+
+}
