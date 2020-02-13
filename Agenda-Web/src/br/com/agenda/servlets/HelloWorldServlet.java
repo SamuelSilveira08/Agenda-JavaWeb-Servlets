76f851d9d0382a577b524f61096d20ce8e2c5282
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-13 18:04:10 -0300

    Created HelloWorldServlet with overridden service method. Servlet mapping done.

--- Agenda-Web/src/br/com/agenda/servlets/HelloWorldServlet.java
+++ Agenda-Web/src/br/com/agenda/servlets/HelloWorldServlet.java
@@ -1,5 +1,31 @@
 package br.com.agenda.servlets;
 
-public class HelloWorldServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8910149643828151646L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Hello World from Servlet </title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p style='font-weight: bold;'> Hello World, I came from Servlet </p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
 }
