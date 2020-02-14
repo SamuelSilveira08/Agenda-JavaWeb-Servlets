b8db53af35d216c424b4ad7a4b4e3bb599cd98b8
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-14 17:37:49 -0300

    Registered JDBC Driver on ConnectionFactory because of Tomcat

--- Agenda-Repositorios/src/br/com/agenda/repositorios/factories/ConnectionFactory.java
+++ Agenda-Repositorios/src/br/com/agenda/repositorios/factories/ConnectionFactory.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	public static Connection getConnection() throws SQLException {
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");
 		return connection;
 	}
