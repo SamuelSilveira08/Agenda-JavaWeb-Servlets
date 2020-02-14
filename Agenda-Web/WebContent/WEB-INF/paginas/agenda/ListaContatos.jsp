59db2e9a09c895ac5512c4cd25b633db66661da1
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-14 17:10:19 -0300

    Done ListaContatosServlet

---
+++ Agenda-Web/WebContent/WEB-INF/paginas/agenda/ListaContatos.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="jstl" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Lista de Contatos</title>
+</head>
+<body>
+	<h1>Lista de Contatos</h1>
+	<table>
+		<tr>
+			<th>Nome</th>
+			<th>Idade</th>
+			<th>Telefone</th>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ Agenda-Web/src/br/com/agenda/servlets/agenda/ListaContatosServlet.java
@@ -0,0 +1,41 @@
+package br.com.agenda.servlets.agenda;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.agenda.entidades.Contato;
+import br.com.agenda.repositorios.impl.ContatoRepositorioJdbc;
+import br.com.agenda.repositorios.interfaces.AgendaRepositorio;
+
+@WebServlet(urlPatterns = {"/agenda/listar"})
+public class ListaContatosServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7261759251496522181L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		AgendaRepositorio<Contato> repositorio = new ContatoRepositorioJdbc();
+		try {
+			List<Contato> contatos = repositorio.selectAll();
+			req.setAttribute("contatos", contatos);
+		} catch (SQLException e) {
+			req.setAttribute("mensagemErro", e.getMessage());
+		}
+		RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/WEB-INF/paginas/agenda/ListaContatos.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	
+
+}
